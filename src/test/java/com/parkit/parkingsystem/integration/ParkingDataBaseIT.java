5a77f0aa5a1e276b19893106d11f4575d51097d8
Benmammar <bader.benmaamar@gmail.com>
2023-06-20 23:34:44 +0100

    etape 6

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,6 +1,6 @@
 package com.parkit.parkingsystem.integration;
 
-import com.parkit.parkingsystem.constants.DBConstants;
+
 import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
@@ -9,34 +9,27 @@ import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
-import java.sql.*;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 import org.junit.jupiter.api.AfterAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
+
 
 import org.mockito.Mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+
 import org.mockito.junit.jupiter.MockitoExtension;
 import static org.mockito.Mockito.when;
-import org.mockito.junit.jupiter.MockitoSettings;
-import org.mockito.quality.Strictness;
+
 
 @ExtendWith(MockitoExtension.class)
-@MockitoSettings(strictness = Strictness.LENIENT)
+//@MockitoSettings(strictness = Strictness.LENIENT)
 public class ParkingDataBaseIT {
 
     private static final DataBaseTestConfig dataBaseTestConfig = new DataBaseTestConfig();
@@ -46,18 +39,10 @@ public class ParkingDataBaseIT {
 
     @Mock
     private static InputReaderUtil inputReaderUtil;
-    
-    @Mock
-    private static Connection connection;
-    
-    @Mock
-    private static PreparedStatement  preparedStatement;
-    
-    @Mock
-    private static ResultSet resultSet;
 
-    @BeforeAll 
-    private static void setUp() throws Exception{ 
+
+    @BeforeAll
+    private static void setUp() throws Exception {
         parkingSpotDAO = new ParkingSpotDAO();
         parkingSpotDAO.dataBaseConfig = dataBaseTestConfig;
         ticketDAO = new TicketDAO();
@@ -73,11 +58,9 @@ public class ParkingDataBaseIT {
     }
 
     @AfterAll
-    private static void tearDown(){
+    private static void tearDown() {
 
     }
-
-   
 
     /*
     @Test
@@ -90,33 +73,16 @@ public class ParkingDataBaseIT {
      */
     // un ticket est actuellement enregistré dans 
     // la base de données et la table du parking est mise à jour avec la disponibilité
-  
     @Test
-    public void testParkingACar() {
-        try {
+    public void testParkingACar(){
+            assertEquals(0, ticketDAO.getNbTicket("ABCDEF"));
             ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
             parkingService.processIncomingVehicle();
+            assertEquals(1, ticketDAO.getNbTicket("ABCDEF"));
+            Ticket savedTicket = ticketDAO.getTicket("ABCDEF");
+            ParkingSpot parkingSpot = savedTicket.getParkingSpot();
+            assertEquals(false, parkingSpot.isAvailable());
 
-            // vérifier que le ticket a été enregistré dans la base de données.
-            verify(ticketDAO, times(1)).saveTicket(any(Ticket.class));
-
-            // vérifier que l'état de disponibilité de la place de stationnement a été mis à jour.
-            verify(parkingSpotDAO, times(1)).updateParking(any(ParkingSpot.class));
-
-            // crée un objet savedTicket avec les valeurs attendues pour les attributs 
-            Ticket savedTicket = new Ticket();
-            savedTicket.setParkingSpot(new ParkingSpot(1, ParkingType.CAR, false));
-            savedTicket.setVehicleRegNumber("ABCDEF");
-            savedTicket.setPrice(0.0);
-            savedTicket.setInTime(new Date(System.currentTimeMillis() - (29 * 60 * 1000)));
-            savedTicket.setOutTime(null);
-
-            // vérifier que les valeurs enregistrées dans la base de données correspondent aux valeurs attendues.
-            verify(ticketDAO, times(1)).saveTicket(eq(savedTicket));
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     /*
@@ -128,43 +94,34 @@ public class ParkingDataBaseIT {
         //TODO: check that the fare generated and out time are populated correctly in the database
     }
      */
-    
 // vérifier que le tarif et l'heure de départ générés sont correctement 
 // renseignés dans la base de données
-    
     @Test
     public void testParkingLotExit() {
-        try {
             testParkingACar();
             ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
             Ticket existingTicket = new Ticket();
-            existingTicket.setInTime(new Date(System.currentTimeMillis() - (60 * 60 * 1000)));  // Heure d'entrée il y a une heure
-
+            existingTicket.setInTime(new Date(System.currentTimeMillis() - (29 * 60 * 1000)));  // Heure d'entrée il y a une heure
             // processExitingVehicle pour simuler le départ de l'utilisateur récurrent
             parkingService.processExitingVehicle();
-
             Date outTime = new Date();
             existingTicket.setOutTime(outTime);
-            
             // Vérifier que le ticket a été mis à jour avec le bon prix
             Ticket updatedTicket = ticketDAO.getTicket("ABCDEF");
-            double expectedPrice = 1.5;
-            assertEquals(expectedPrice, updatedTicket.getPrice());
-
-            // Vérifier que le ticket a été mis à jour avec l'heure de sortie
-            assertEquals(outTime, updatedTicket.getOutTime());
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            double expectedPrice = 0;
+            assertEquals(expectedPrice, updatedTicket.getPrice());     
+            // Vérifier que le ticket a été mis à jour avec l'heure de sortie  
+            SimpleDateFormat dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.ENGLISH);
+            String expectedOutTime = dateFormat.format(outTime);
+            String bddOutTime = dateFormat.format(updatedTicket.getOutTime());
+            assertEquals(expectedOutTime, bddOutTime);
+            //   assertEquals(outTime, updatedTicket.getOutTime());
     }
 
 // tester le calcul du prix d’un ticket via l’appel de processIncomingVehicle 
 // et processExitingVehicle dans le cas d’un utilisateur récurrent
-    
     @Test
     public void testParkingLotExitRecurringUser() {
-        try {
             ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
             parkingService.processIncomingVehicle();
             // Simuler un utilisateur récurrent en ayant déjà un ticket dans la base de données
@@ -175,57 +132,11 @@ public class ParkingDataBaseIT {
             existingTicket.setInTime(new Date(System.currentTimeMillis() - (60 * 60 * 1000)));  // Heure d'entrée il y a une heure
             existingTicket.setOutTime(null);
             ticketDAO.saveTicket(existingTicket);
-
             // processExitingVehicle pour simuler le départ de l'utilisateur récurrent
             parkingService.processExitingVehicle();
-
             // Vérifier que le ticket a été mis à jour avec le bon prix (avec remise de 5%)
             Ticket updatedTicket = ticketDAO.getTicket("ABCDEF");
             double expectedPrice = 1.5 * 0.95;
-            assertEquals(expectedPrice, updatedTicket.getPrice());
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            assertEquals(expectedPrice, updatedTicket.getPrice(), 0.01); 
     }
-    
-    
-   
-@Test
-    public void testSaveTicket() throws Exception {
-        Ticket ticket = new Ticket();
-        ticket.setParkingSpot(new ParkingSpot(1, ParkingType.CAR, false));
-        ticket.setVehicleRegNumber("ABCDEF");
-        ticket.setPrice(10.0);
-        ticket.setInTime(new Timestamp(System.currentTimeMillis()));
-       when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
-        when(preparedStatement.execute()).thenReturn(true);
-        assertEquals(true, ticketDAO.saveTicket(ticket));
-    }
-
-    @Test
-    public void testGetTicket() throws SQLException, ClassNotFoundException {
-      //  when(dataBaseTestConfig.getConnection()).thenReturn(connection);
-        when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
-        when(preparedStatement.executeQuery()).thenReturn(resultSet);
-        when(resultSet.next()).thenReturn(true);
-        when(resultSet.getInt(anyInt())).thenReturn(1);
-        when(resultSet.getString(anyInt())).thenReturn("ABCDEF");
-        when(resultSet.getDouble(anyInt())).thenReturn(10.0);
-        when(resultSet.getTimestamp(anyInt())).thenReturn(new Timestamp(System.currentTimeMillis()));
-        String vehicleRegNumber = "ABCDEF";
-        assertNotNull(ticketDAO.getTicket(vehicleRegNumber));
-    }
-    
-    @Test
-    public void testUpdateTicket() throws SQLException, ClassNotFoundException {
-        when(connection.prepareStatement(DBConstants.UPDATE_TICKET)).thenReturn(preparedStatement);
-        when(preparedStatement.execute()).thenReturn(true);  
-        Ticket ticket = new Ticket();
-        ticket.setPrice(10.0);
-        ticket.setOutTime(new Date());
-        ticket.setId(1);
-        assertEquals(true, ticketDAO.updateTicket(ticket));
-    }
-
 }
